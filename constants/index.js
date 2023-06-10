5abc1f3d7a6ec69960e7a61e6c1484a900cb3303
Nayan <100008163+Nayanchandrakar@users.noreply.github.com>
2023-06-10 13:44:03 +0530

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -2,7 +2,7 @@ export  const toogleLinks = [
     {field:'Social-Mission', href:"/"},
     {field:'Digital Platforms', href:"/Platform"},
     {field:'About us', href:"/about"},
-    {field:'Trainers', href:"/training"},
+    {field:'Training', href:"/training"},
     {field:'Team', href:"/team/core_team"},
     {field:"Testimonials", href:"/testimonials"},
     {field:'Careers', href:"/careers"},
